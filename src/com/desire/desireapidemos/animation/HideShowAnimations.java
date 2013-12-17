4ed9fa72e1ebea07a866ab495ec753bf24c2c8e5
desire <room.desire@gmail.com>
2013-12-17 11:50:51 +0800

    [add] hide show animation layout & activity.

---
+++ src/com/desire/desireapidemos/animation/HideShowAnimations.java
@@ -0,0 +1,16 @@
+package com.desire.desireapidemos.animation;
+
+import com.desire.desireapidemos.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HideShowAnimations extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hide_show_animations);
+	}
+
+}
